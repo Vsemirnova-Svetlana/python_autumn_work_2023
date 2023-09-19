7c4b2810e1aa959039e1cbe8bf8185f3f327a879
SvetlanaV <svetlana-comilfo@ya.ru>
2023-09-19 17:39:08 +0300

    factorial.py

---
+++ factorial.py
@@ -0,0 +1,21 @@
+n = int(input("Введите число: "))
+
+factorial = 1
+for i in range(1,n+1):
+    factorial = factorial * i
+    i +=1
+string = list(str(factorial))
+new_string = string[::-1]
+count = 0
+
+for i in new_string[:]:
+    if i == "0":
+        count +=1
+    else:
+        break
+
+
+print(count)
+
+print(string)
+print(new_string)
