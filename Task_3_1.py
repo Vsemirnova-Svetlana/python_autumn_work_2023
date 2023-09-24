4c9d5fb94e9aae3410b617436e1790e6a0045159
Vsemirnova-Svetlana <145149965+Vsemirnova-Svetlana@users.noreply.github.com>
2023-09-24 11:30:16 +0300

    Update Task_3_1.py

--- Task_3_1.py
+++ Task_3_1.py
@@ -1,16 +1,13 @@
-lst = []
+total_salary = 0
+num_of_workers = 0
+
 while True:
     salary = int(input('Введите зарплату, для завершения ввода нажмите 0 ("ноль"): '))
     if salary == 0: break
 
     else:
-        lst.append(salary)
-
-total_salary = 0
-num_of_workers = 0
-for j in lst:
-    num_of_workers +=1
-    total_salary +=j
+        total_salary = total_salary + salary
+        num_of_workers +=1
 
 average_sal = total_salary/num_of_workers
 print("Средняя зарплата сотрудников: ", round(average_sal,1))
