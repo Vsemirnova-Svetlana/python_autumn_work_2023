930c92a4eea46d52c2406b8db8d6df0c0b021d54
Vsemirnova-Svetlana <145149965+Vsemirnova-Svetlana@users.noreply.github.com>
2023-09-25 13:21:10 +0300

    Update Task_3_2.py

--- Task_3_2.py
+++ Task_3_2.py
@@ -1,16 +1,19 @@
 n = input("Введите число: ")
-n0,n1,n2,n3,n4,n5,n6,n7,n8,n9 = 0,0,0,0,0,0,0,0,0,0
+lst = []
 for i in n:
-    if i == '0': n0 +=1
-    elif i == '1': n1 +=1
-    elif i == '2': n2 +=1
-    elif i == '3': n3 +=1
-    elif i == '4': n4 +=1
-    elif i == '5': n5 +=1
-    elif i == '6': n6 +=1
-    elif i == '7': n7 +=1
-    elif i == '8': n8 +=1
-    elif i == '9': n9 +=1
+    lst.append(i)
+
+n0 = lst.count('0')
+n1 = lst.count('1')
+n2 = lst.count('2')
+n3 = lst.count('3')
+n4 = lst.count('4')
+n5 = lst.count('5')
+n6 = lst.count('6')
+n7 = lst.count('7')
+n8 = lst.count('8')
+n9 = lst.count('9')
+
 print(f'0 - {n0}\n1 - {n1}\n2 - {n2}\n3 - {n3}\n'
       f'4 - {n4}\n5 - {n5}\n6 - {n6}\n7 - {n7}\n'
       f'8 - {n8}\n9 - {n9}\n')
